61c86e80fe238c321431aca44d047cd479bd790d
Arjen <arjen_swellengrebel@msn.com>
2013-04-20 19:38:32 +0200

    I don't actually agree with this design. But, graph-drawing is back in MainScreen now.

--- src/com/example/meterkast/MainScreen.java
+++ src/com/example/meterkast/MainScreen.java
@@ -7,8 +7,13 @@ import android.content.DialogInterface;
 import android.content.Intent;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 
 import android.graphics.Bitmap.CompressFormat;
+import android.graphics.Paint.Align;
 
 import android.graphics.BitmapFactory;
 
@@ -96,16 +101,63 @@ public class MainScreen extends Activity {
     	Bitmap graph = BitmapFactory.decodeResource(getResources(), R.drawable.grid);
         graph = Bitmap.createScaledBitmap(graph, WIDTH, HEIGHT, false);
     	// I just make the data do it right now; it has access to the necessary data to do it.
-    	graph = data.drawBarGraphs(WIDTH, HEIGHT, graph);
+    	graph = drawBarGraphs(WIDTH, HEIGHT, graph);
         this.imageView1.setImageBitmap(graph);
     }
 
+    
+    /**
+	 *  Similar to the drawGraphs function, but drawing the daily usage (one bar each day.)
+	 */
+	public Bitmap drawBarGraphs(int width, int height, Bitmap graph) { 
+		// It takes a graph because it will be drawing over previously drawn stuff.
+		int noRecs = data.getSettingSelection("nOfRecords");
+        long startTime = data.getDate(1); // Again harvesting some values first.
+        long endTime = data.getEndDate();
+        long duration = endTime - startTime;
+        int totalDays = (int) (duration / RecordingData.MILLISINADAY);
+        int highestRecorded = 0;
+
+        // Find the highest of all recordings...
+        for (int i = 1; i <= noRecs; i++) {
+            String frst = "frst";
+			int thisRecordingFRST = data.getRecording(i, frst);
+            int thisRecordingSCND = data.getRecording(i, "scnd");
+
+            if (thisRecordingFRST + thisRecordingSCND > highestRecorded) { // The highest of all TOTALS, that is
+                highestRecorded = thisRecordingFRST + thisRecordingSCND;
+            }
+        }
+
+        for (int day = 0; day < totalDays; day += 1) {
+            long morning = startTime + (day * RecordingData.MILLISINADAY); // Starting time for the beginning of the current day
+            long evening = startTime + (day + 1) * RecordingData.MILLISINADAY; // And 24 hours later, or the end
+            int usage = data.thisDaysUsage(morning, evening);
+
+            // Fractions are between 0 and 1, and they help us view the image in percentages.
+            double morningWidthFraction = ((double) morning / (double) duration);
+            double eveningWidthFraction = ((double) evening / (double) duration);
+            double daysRecFrac = (((double) usage) / ((double) highestRecorded));
+
+            // Then convert them to pixels by multiplying with the image's width.
+            int morningWidth = (int) (morningWidthFraction * width);
+            int eveningWidth = (int) (eveningWidthFraction * width);
+            int usageHeight = height - (int) (daysRecFrac * height);
+
+            Canvas canvas = new Canvas(graph);
+            Paint p = new Paint();
+            p.setColor(Color.RED);// And for each day, I draw one rectangle for its usage.
+            canvas.drawRect(new Rect(morningWidth, usageHeight, eveningWidth, height), p);
+        }
+        
+        return graph;
+	}
     
     // Draws bar graphs on the picture being displayed. These graphs are SUPER USEFUL.
     private void graphRecordings() {
     	Bitmap graph = BitmapFactory.decodeResource(getResources(), R.drawable.grid);
         graph = Bitmap.createScaledBitmap(graph, WIDTH, HEIGHT, false);
-        graph = data.drawGraph(WIDTH, HEIGHT, graph); // Make data do it, because it has all the information for that.
+        graph = drawGraph(WIDTH, HEIGHT, graph); // Make data do it, because it has all the information for that.
 
         this.imageView1.setImageBitmap(graph);
     }
@@ -179,5 +231,100 @@ public class MainScreen extends Activity {
 
         // And SHARE!
         startActivity(Intent.createChooser(share, getString(R.string.sharewindow_title)));
+    }
+    
+ // Draw a graph showing the progression of electricity usage in this household.
+    public Bitmap drawGraph(int width, int height, Bitmap graph) {
+
+        int noRecs = data.getSettingSelection("nOfRecords");
+
+        int highestRecorded = 0;
+        int lowestRecorded = RecordingData.HIGHESTPOSSIBLERECORDING;
+        
+        // Find the highest recording and the lowest recording.
+        // These values are used to determine the bounding of the image.
+        for (int i = 1; i <= noRecs; i++) {
+            int thisRecordingFRST = data.getRecording(i, "frst");
+            int thisRecordingSCND = data.getRecording(i, "scnd");
+
+            if (thisRecordingFRST + thisRecordingSCND > highestRecorded) {
+            	// Keep updating the values. 
+                highestRecorded = thisRecordingFRST + thisRecordingSCND;
+            }
+
+            if (thisRecordingFRST < lowestRecorded) {
+                lowestRecorded = thisRecordingFRST; 
+            }
+
+            if (thisRecordingSCND < lowestRecorded) {
+                lowestRecorded = thisRecordingSCND;
+            }
+        }
+
+        // Determine the necessary variables. These are used for the left and right bounds of the image.
+        long startTime = data.getDate(1);
+        long endTime = data.getDate(Integer.valueOf(noRecs));
+
+        int previousTime = 0;
+        int duration = (int) endTime - (int) startTime;
+        double recordingSpan = highestRecorded - lowestRecorded;
+
+        // For the second recording and all recordings after it:
+        for (int i = 2; i <= noRecs; i++) { // Start at two because we consider recordings in pairs, each with its preceding.
+            
+        	// In the coming block I'm going to calculate a whole load of variables...
+        	int thisTime = (int) (data.getDate(i) - startTime);
+            int thisRecordingFRST = data.getRecording(i, "frst");
+            int thisRecordingSCND = data.getRecording(i,"scnd");
+            int iPrev = (i - 1);
+            int previousRecordingFRST = data.getRecording(iPrev ,"frst");
+            int previousRecordingSCND = data.getRecording(iPrev , "scnd");
+
+            // This is the part of the image the time falls on:
+            double previousWidthFraction = ((double) previousTime / (double) duration);
+            double thisWidthFraction = ((double) thisTime / (double) duration);
+
+            // And the part of the image the recording's value falls on. 0 <= X <= 1
+            double previousRecFractionFRST = (((double) previousRecordingFRST - (double) lowestRecorded) / recordingSpan);
+            double previousRecFractionSCND = (((double) previousRecordingSCND - (double) lowestRecorded) / recordingSpan);
+            double thisRecFractionFRST = (((double) thisRecordingFRST - (double) lowestRecorded) / recordingSpan);
+            double thisRecFractionSCND = (((double) thisRecordingSCND - (double) lowestRecorded) / recordingSpan);
+
+            // And based on the fractions, here are the pixels that make the locations on the image:
+            int drawX1 = (int) (width * previousWidthFraction);
+            int drawX2 = (int) (width * thisWidthFraction);
+
+            int drawY1FRST = (int) (height - (height * previousRecFractionFRST));
+            int drawY1SCND = (int) (height - (height * previousRecFractionSCND));
+            int drawY1COMBO = (int) (height - (height * (previousRecFractionSCND + previousRecFractionFRST)));
+            int drawY2FRST = (int) (height - (height * thisRecFractionFRST));
+            int drawY2SCND = (int) (height - (height * thisRecFractionSCND));
+            int drawY2COMBO = (int) (height - (height * (thisRecFractionSCND + thisRecFractionFRST)));
+
+            // Now then, drawing:
+            Canvas canvas = new Canvas(graph);
+            Paint p = new Paint();
+            p.setColor(Color.BLUE); // Blue is nightly usage;
+            // Draw lines between two datapoints. Each datapoint is some combinations of the above variables.
+            canvas.drawLine(drawX1, drawY1SCND, drawX2, drawY2SCND, p);
+            canvas.drawLine(drawX1, drawY1SCND - 1, drawX2, drawY2SCND - 1, p);
+            p.setColor(Color.GREEN); // Green is daily usage,
+            canvas.drawLine(drawX1, drawY1FRST, drawX2, drawY2FRST, p);
+            canvas.drawLine(drawX1, drawY1FRST - 1, drawX2, drawY2FRST - 1, p);
+            p.setColor(Color.BLUE);// And here I draw both colors once, meaning this is combined usage.
+            canvas.drawLine(drawX1, drawY1COMBO, drawX2, drawY2COMBO, p);
+            p.setColor(Color.GREEN);
+            canvas.drawLine(drawX1, drawY1COMBO - 1, drawX2, drawY2COMBO - 1, p);
+
+            p.setColor(Color.RED);
+            p.setTextSize(30);
+            p.setTextAlign(Align.CENTER);
+            // Then I note the upper and lower bounds of the recordings in the image, for reference.
+            canvas.drawText(Integer.toString(highestRecorded), width / 2, RecordingData.TOPTEXTDISTANCE, p);
+            canvas.drawText(Integer.toString(lowestRecorded), width / 2, height - RecordingData.BOTTOMTEXTDISTANCE, p);
+
+            previousTime = thisTime; // Update the time for the loop. Then do it again.
+        }
+		return graph;
     }
 }
--- src/com/example/meterkast/RecordingData.java
+++ src/com/example/meterkast/RecordingData.java
@@ -4,12 +4,6 @@ import java.io.File;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
-import android.graphics.Paint.Align;
 import android.os.Environment;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -19,10 +13,10 @@ public class RecordingData {
 	private static final String KINDOFMETER = "Metersoort";
 	private static final String NUMBEROFOCCUPANTS = "Inwoneraantal";
 	private static final String TYPEOFHOUSE = "Woonsituatie";
-	private static final int MILLISINADAY = 1000 * 60 * 60 * 24;
-	private static final int BOTTOMTEXTDISTANCE = 10;
-	private static final int TOPTEXTDISTANCE = 30;
-	private static final int HIGHESTPOSSIBLERECORDING = 999999 * 2;
+	static final int MILLISINADAY = 1000 * 60 * 60 * 24;
+	static final int BOTTOMTEXTDISTANCE = 10;
+	static final int TOPTEXTDISTANCE = 30;
+	static final int HIGHESTPOSSIBLERECORDING = 999999 * 2;
 
 	private static final int HASNOTBEENRECORDED = -1;
 	public SharedPreferences recordings;
@@ -63,100 +57,7 @@ public class RecordingData {
     	return recordings.getLong(i+"date"+getCurrentUser(), HASNOTBEENRECORDED);
     }
 
-    // Draw a graph showing the progression of electricity usage in this household.
-    public Bitmap drawGraph(int width, int height, Bitmap graph) {
-
-        int noRecs = getSettingSelection("nOfRecords");
-
-        int highestRecorded = 0;
-        int lowestRecorded = HIGHESTPOSSIBLERECORDING;
-        
-        // Find the highest recording and the lowest recording.
-        // These values are used to determine the bounding of the image.
-        for (int i = 1; i <= noRecs; i++) {
-            int thisRecordingFRST = getRecording(i, "frst");
-            int thisRecordingSCND = getRecording(i, "scnd");
-
-            if (thisRecordingFRST + thisRecordingSCND > highestRecorded) {
-            	// Keep updating the values. 
-                highestRecorded = thisRecordingFRST + thisRecordingSCND;
-            }
-
-            if (thisRecordingFRST < lowestRecorded) {
-                lowestRecorded = thisRecordingFRST; 
-            }
-
-            if (thisRecordingSCND < lowestRecorded) {
-                lowestRecorded = thisRecordingSCND;
-            }
-        }
-
-        // Determine the necessary variables. These are used for the left and right bounds of the image.
-        long startTime = getDate(1);
-        long endTime = getDate(Integer.valueOf(noRecs));
-
-        int previousTime = 0;
-        int duration = (int) endTime - (int) startTime;
-        double recordingSpan = highestRecorded - lowestRecorded;
-
-        // For the second recording and all recordings after it:
-        for (int i = 2; i <= noRecs; i++) { // Start at two because we consider recordings in pairs, each with its preceding.
-            
-        	// In the coming block I'm going to calculate a whole load of variables...
-        	int thisTime = (int) (getDate(i) - startTime);
-            int thisRecordingFRST = getRecording(i, "frst");
-            int thisRecordingSCND = getRecording(i,"scnd");
-            int iPrev = (i - 1);
-            int previousRecordingFRST = getRecording(iPrev ,"frst");
-            int previousRecordingSCND = getRecording(iPrev , "scnd");
-
-            // This is the part of the image the time falls on:
-            double previousWidthFraction = ((double) previousTime / (double) duration);
-            double thisWidthFraction = ((double) thisTime / (double) duration);
-
-            // And the part of the image the recording's value falls on. 0 <= X <= 1
-            double previousRecFractionFRST = (((double) previousRecordingFRST - (double) lowestRecorded) / recordingSpan);
-            double previousRecFractionSCND = (((double) previousRecordingSCND - (double) lowestRecorded) / recordingSpan);
-            double thisRecFractionFRST = (((double) thisRecordingFRST - (double) lowestRecorded) / recordingSpan);
-            double thisRecFractionSCND = (((double) thisRecordingSCND - (double) lowestRecorded) / recordingSpan);
-
-            // And based on the fractions, here are the pixels that make the locations on the image:
-            int drawX1 = (int) (width * previousWidthFraction);
-            int drawX2 = (int) (width * thisWidthFraction);
-
-            int drawY1FRST = (int) (height - (height * previousRecFractionFRST));
-            int drawY1SCND = (int) (height - (height * previousRecFractionSCND));
-            int drawY1COMBO = (int) (height - (height * (previousRecFractionSCND + previousRecFractionFRST)));
-            int drawY2FRST = (int) (height - (height * thisRecFractionFRST));
-            int drawY2SCND = (int) (height - (height * thisRecFractionSCND));
-            int drawY2COMBO = (int) (height - (height * (thisRecFractionSCND + thisRecFractionFRST)));
-
-            // Now then, drawing:
-            Canvas canvas = new Canvas(graph);
-            Paint p = new Paint();
-            p.setColor(Color.BLUE); // Blue is nightly usage;
-            // Draw lines between two datapoints. Each datapoint is some combinations of the above variables.
-            canvas.drawLine(drawX1, drawY1SCND, drawX2, drawY2SCND, p);
-            canvas.drawLine(drawX1, drawY1SCND - 1, drawX2, drawY2SCND - 1, p);
-            p.setColor(Color.GREEN); // Green is daily usage,
-            canvas.drawLine(drawX1, drawY1FRST, drawX2, drawY2FRST, p);
-            canvas.drawLine(drawX1, drawY1FRST - 1, drawX2, drawY2FRST - 1, p);
-            p.setColor(Color.BLUE);// And here I draw both colors once, meaning this is combined usage.
-            canvas.drawLine(drawX1, drawY1COMBO, drawX2, drawY2COMBO, p);
-            p.setColor(Color.GREEN);
-            canvas.drawLine(drawX1, drawY1COMBO - 1, drawX2, drawY2COMBO - 1, p);
-
-            p.setColor(Color.RED);
-            p.setTextSize(30);
-            p.setTextAlign(Align.CENTER);
-            // Then I note the upper and lower bounds of the recordings in the image, for reference.
-            canvas.drawText(Integer.toString(highestRecorded), width / 2, TOPTEXTDISTANCE, p);
-            canvas.drawText(Integer.toString(lowestRecorded), width / 2, height - BOTTOMTEXTDISTANCE, p);
-
-            previousTime = thisTime; // Update the time for the loop. Then do it again.
-        }
-		return graph;
-    }
+    
 	
 	public void makeRecording(String field1, String field2, Context context) {
 		// Create an editor object for writing to the settings file.
@@ -212,57 +113,12 @@ public class RecordingData {
 		return getDate(Integer.valueOf(noRecs));
 	}
 
-	/**
-	 *  Similar to the drawGraphs function, but drawing the daily usage (one bar each day.)
-	 */
-	public Bitmap drawBarGraphs(int width, int height, Bitmap graph) { 
-		// It takes a graph because it will be drawing over previously drawn stuff.
-		int noRecs = getSettingSelection("nOfRecords");
-        long startTime = getDate(1); // Again harvesting some values first.
-        long endTime = getEndDate();
-        long duration = endTime - startTime;
-        int totalDays = (int) (duration / MILLISINADAY);
-        int highestRecorded = 0;
-
-        // Find the highest of all recordings...
-        for (int i = 1; i <= noRecs; i++) {
-            String frst = "frst";
-			int thisRecordingFRST = getRecording(i, frst);
-            int thisRecordingSCND = getRecording(i, "scnd");
-
-            if (thisRecordingFRST + thisRecordingSCND > highestRecorded) { // The highest of all TOTALS, that is
-                highestRecorded = thisRecordingFRST + thisRecordingSCND;
-            }
-        }
-
-        for (int day = 0; day < totalDays; day += 1) {
-            long morning = startTime + (day * MILLISINADAY); // Starting time for the beginning of the current day
-            long evening = startTime + (day + 1) * MILLISINADAY; // And 24 hours later, or the end
-            int usage = thisDaysUsage(morning, evening);
-
-            // Fractions are between 0 and 1, and they help us view the image in percentages.
-            double morningWidthFraction = ((double) morning / (double) duration);
-            double eveningWidthFraction = ((double) evening / (double) duration);
-            double daysRecFrac = (((double) usage) / ((double) highestRecorded));
-
-            // Then convert them to pixels by multiplying with the image's width.
-            int morningWidth = (int) (morningWidthFraction * width);
-            int eveningWidth = (int) (eveningWidthFraction * width);
-            int usageHeight = height - (int) (daysRecFrac * height);
-
-            Canvas canvas = new Canvas(graph);
-            Paint p = new Paint();
-            p.setColor(Color.RED);// And for each day, I draw one rectangle for its usage.
-            canvas.drawRect(new Rect(morningWidth, usageHeight, eveningWidth, height), p);
-        }
-        
-        return graph;
-	}
+	
 	
 	/**
 	 * Find out how much electricity has been used between two moments.
 	 */
-	private int thisDaysUsage(long morning, long evening) {
+	int thisDaysUsage(long morning, long evening) {
         int noRecs = getSettingSelection("nOfRecords");
         long thisDate;
         int beforeMorning = 0; // These will be set later
